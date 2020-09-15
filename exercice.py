6c27590d5ded1d386b20d526047db17f7563dfe3
ghalich <ghali.chraibi@polymtl.ca>
2020-09-15 23:10:54 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -30,7 +30,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     counter
-    for i in range(len(string)):
+    for i in range(len(sentence)):
         if string[i:i+len(old_char)] == old_char:
 
 
