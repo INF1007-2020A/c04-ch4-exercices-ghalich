596d6d7a6201a377da49de25fe23c9cfe48f9431
ghalich <ghali.chraibi@polymtl.ca>
2020-09-15 22:46:32 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -3,10 +3,9 @@
 
 
 def is_even_len(string: str) -> bool:
-    
     if (len(string) % 2) == 0:
         return True
-    pass
+    else: return False
 
 
 def remove_third_char(string: str) -> str:
