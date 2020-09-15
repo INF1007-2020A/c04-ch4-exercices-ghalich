e1c460608478d88133258d774a2239adb226b8e5
ghalich <ghali.chraibi@polymtl.ca>
2020-09-15 22:47:07 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    return string[0:2]
+    return string[0:2]+string[3:]
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
